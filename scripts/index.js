7c82e0bbb6242c01da69934c07d362462131f3d2
STNyame <sentwalidors@hotmail.com>
2021-04-21 09:46:37 +0200

    skeleton webpage

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,21 @@
 // alert("working!")
+let index = 0;
+document.querySelector("#dark-mode").addEventListener("click", () => {
+  console.log("working");
+  document.querySelector("body").classList.toggle("body-dark");
+  document.querySelector(".copyright").classList.toggle("cdark");
+  document.querySelector(".header-change").classList.toggle("cdark");
+
+  if (index === 0) {
+    index++;
+
+    document
+      .querySelector("#dark-mode")
+      .setAttribute("src", "images/002-moon.png");
+  } else {
+    index--;
+    document
+      .querySelector("#dark-mode")
+      .setAttribute("src", "images/001-sun.png");
+  }
+});
